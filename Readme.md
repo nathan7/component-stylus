d43e2c4a99c11dbfed85cba073569874a624e616
Nathan Zadoks <nathan@nathan7.eu>
2013-05-29 22:02:57 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# component-stylus
+
+  Component plugin to compiles your Stylus.
+
+## Usage
+
+  ``component build -u component-stylus`` or use it through builder.
+
---
+++ index.js
@@ -0,0 +1,37 @@
+var fs = require('fs')
+  , literally = require('literally')
+  , async = require('async')
+  , Stylus = require('stylus')
+  , Nib = function() {}
+try { nib = require('nib') } catch (e) {}
+
+module.exports = function(builder) { builder.hook('before styles', build.bind(builder)) }
+
+function build(pkg, next) {
+  var builder = this
+  async.each((pkg.config.styles || []).filter(isStylus), function(filename, cb) {
+    async.waterfall(
+      [ literally.async(filename)
+      , readFile
+      , compileFile
+      , addFile
+      ]
+    , cb)
+
+    function compileFile(stylus, cb) {
+      Stylus(stylus)
+        .set('filename', filename)
+        .set('compress', !builder.dev)
+        .use(Nib)
+        .render(cb)
+    }
+
+    function addFile(css, cb) {
+      pkg._files[filename] = css
+      cb()
+    }
+  }, next)
+}
+
+function readFile(fname, cb) { fs.readFile(fname, 'utf8', cb) }
+function isStylus(fname) { return !!fname.match(/\.styl$/) }
